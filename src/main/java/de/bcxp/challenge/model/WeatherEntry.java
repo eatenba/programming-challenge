87676ce622fa5df0e2f2356931e8c5498fe016c9
eatenba <iappreview@web.de>
2025-03-30 21:50:21 -0700

    Removed unnecessary columns (declutter).

--- src/main/java/de/bcxp/challenge/model/WeatherEntry.java
+++ src/main/java/de/bcxp/challenge/model/WeatherEntry.java
@@ -8,17 +8,6 @@ public class WeatherEntry {
     private int day;
     private int maximumTemperature;
     private int minimumTemperature;
-    private int averageTemperature;
-    private double averageDewPoint;
-    private int maximumPrecepationInOneHourDuringDay;
-    private int persiannDynamicInfraredRainRate;
-    private double averageWindSpeed;
-    private int windDirection;
-    private int maximumWindSpeed;
-    private double skyCover;
-    private int maximumRelativeHumidity;
-    private int minimumRelativeHumidity;
-    private double averageSeaLevelPressure;
 
     public int getDay() {
         return day;
@@ -42,93 +31,5 @@ public class WeatherEntry {
 
     public void setMinimumTemperature(int minimumTemperature) {
         this.minimumTemperature = minimumTemperature;
-    }
-
-    public int getAverageTemperature() {
-        return averageTemperature;
-    }
-
-    public void setAverageTemperature(int averageTemperature) {
-        this.averageTemperature = averageTemperature;
-    }
-
-    public double getAverageDewPoint() {
-        return averageDewPoint;
-    }
-
-    public void setAverageDewPoint(double averageDewPoint) {
-        this.averageDewPoint = averageDewPoint;
-    }
-
-    public int getMaximumPrecepationInOneHourDuringDay() {
-        return maximumPrecepationInOneHourDuringDay;
-    }
-
-    public void setMaximumPrecepationInOneHourDuringDay(int maximumPrecepationInOneHourDuringDay) {
-        this.maximumPrecepationInOneHourDuringDay = maximumPrecepationInOneHourDuringDay;
-    }
-
-    public int getPersiannDynamicInfraredRainRate() {
-        return persiannDynamicInfraredRainRate;
-    }
-
-    public void setPersiannDynamicInfraredRainRate(int persiannDynamicInfraredRainRate) {
-        this.persiannDynamicInfraredRainRate = persiannDynamicInfraredRainRate;
-    }
-
-    public double getAverageWindSpeed() {
-        return averageWindSpeed;
-    }
-
-    public void setAverageWindSpeed(double averageWindSpeed) {
-        this.averageWindSpeed = averageWindSpeed;
-    }
-
-    public int getWindDirection() {
-        return windDirection;
-    }
-
-    public void setWindDirection(int windDirection) {
-        this.windDirection = windDirection;
-    }
-
-    public int getMaximumWindSpeed() {
-        return maximumWindSpeed;
-    }
-
-    public void setMaximumWindSpeed(int maximumWindSpeed) {
-        this.maximumWindSpeed = maximumWindSpeed;
-    }
-
-    public double getSkyCover() {
-        return skyCover;
-    }
-
-    public void setSkyCover(double skyCover) {
-        this.skyCover = skyCover;
-    }
-
-    public int getMaximumRelativeHumidity() {
-        return maximumRelativeHumidity;
-    }
-
-    public void setMaximumRelativeHumidity(int maximumRelativeHumidity) {
-        this.maximumRelativeHumidity = maximumRelativeHumidity;
-    }
-
-    public int getMinimumRelativeHumidity() {
-        return minimumRelativeHumidity;
-    }
-
-    public void setMinimumRelativeHumidity(int minimumRelativeHumidity) {
-        this.minimumRelativeHumidity = minimumRelativeHumidity;
-    }
-
-    public double getAverageSeaLevelPressure() {
-        return averageSeaLevelPressure;
-    }
-
-    public void setAverageSeaLevelPressure(double averageSeaLevelPressure) {
-        this.averageSeaLevelPressure = averageSeaLevelPressure;
     }
 }
