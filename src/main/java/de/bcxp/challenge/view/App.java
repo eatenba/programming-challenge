18e29a410cf7e42d0bd4634182770773d02883df
eatenba <iappreview@web.de>
2025-03-31 08:18:48 -0700

    Added execution code for view

--- src/main/java/de/bcxp/challenge/view/App.java
+++ src/main/java/de/bcxp/challenge/view/App.java
@@ -1,5 +1,9 @@
 package de.bcxp.challenge.view;
 
+import de.bcxp.challenge.controller.WeatherController;
+import de.bcxp.challenge.controller.handler.CSVEntryHandler;
+import de.bcxp.challenge.model.WeatherEntry;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -13,7 +17,8 @@ public final class App {
     public static void main(String... args) {
 
         // Your preparation code …
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
+        WeatherController weatherController = new WeatherController(new CSVEntryHandler< WeatherEntry >("src/main/resources/de/bcxp/challenge/weather.csv", ',', WeatherEntry.class));
+        int dayWithSmallestTempSpread = weatherController.getSmallestTemperatureSpread();     // Your day analysis function call …
         System.out.printf("Day with smallest temperature spread: %s%n", dayWithSmallestTempSpread);
 
         String countryWithHighestPopulationDensity = "Some country"; // Your population density analysis function call …
