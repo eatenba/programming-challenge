ad41fd0e4e5e5abc059d524e926afe19bffcbbf4
eatenba <iappreview@web.de>
2025-03-30 21:50:49 -0700

    Fixed return type to return a list instead of a single object.

--- src/main/java/de/bcxp/challenge/controller/handler/IWeatherEntryHandler.java
+++ src/main/java/de/bcxp/challenge/controller/handler/IWeatherEntryHandler.java
@@ -2,11 +2,13 @@ package de.bcxp.challenge.controller.handler;
 
 import de.bcxp.challenge.model.WeatherEntry;
 
+import java.util.List;
+
 /**
  * Implement this interface for your own data handling method.
  * TODO:
  * - State further needed methods for handling weather data in the future.
  */
 public interface IWeatherEntryHandler {
-    public WeatherEntry getWeatherEntries();
+    public List<WeatherEntry> getWeatherEntries();
 }
