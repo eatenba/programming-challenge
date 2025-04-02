aedadfc5d8a46e2790a4dfc629b3bc0275d87c16
eatenba <iappreview@web.de>
2025-04-01 18:56:32 -0700

    Removed unused statement.

--- src/test/java/de/bcxp/challenge/controller/handler/WeatherEntryHandlerTests.java
+++ src/test/java/de/bcxp/challenge/controller/handler/WeatherEntryHandlerTests.java
@@ -1,6 +1,5 @@
 package de.bcxp.challenge.controller.handler;
 
-import de.bcxp.challenge.controller.WeatherEntryController;
 import de.bcxp.challenge.model.WeatherEntry;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
