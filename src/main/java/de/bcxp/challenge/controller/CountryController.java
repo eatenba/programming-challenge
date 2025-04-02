640949d41411fb8c17e6455bcd24db1813b6aff2
eatenba <iappreview@web.de>
2025-04-01 17:14:14 -0700

    Finished 2nd assignment. TODO: Document code + add unittests.

--- src/main/java/de/bcxp/challenge/controller/CountryController.java
+++ src/main/java/de/bcxp/challenge/controller/CountryController.java
@@ -13,9 +13,33 @@ public final class CountryController {
         this.countryEntryIHandler = countryEntryIHandler;
     }
 
-    public String getHighestPopulatedCountry(){
+    public String getDensestPopulatedCountry(){
         List<CountryEntry> countryEntries = countryEntryIHandler.getEntries();
-        return "TODO";
+        return computeDensestPopulatedArea(countryEntries);
     }
 
+    protected String computeDensestPopulatedArea(List<CountryEntry> countryEntries){
+        double densestPopulation = Double.MIN_VALUE;
+        String densestPopulatedArea = null;
+        for (int i=0; i<countryEntries.size(); i++){
+            CountryEntry currentEntry = countryEntries.get(i);
+            if(isContenderPopulationDensityHigher(densestPopulation, getCountryEntryPopulationDensity(currentEntry))){
+                densestPopulation = getCountryEntryPopulationDensity(currentEntry);
+                densestPopulatedArea = currentEntry.getName();
+            }
+        }
+        return densestPopulatedArea;
+    }
+
+    protected boolean isContenderPopulationDensityHigher(double currentHighestPopulation, double contenderPopulation){
+        return currentHighestPopulation < contenderPopulation;
+    }
+
+    protected double getCountryEntryPopulationDensity(CountryEntry countryEntry){
+        if(countryEntry.getArea() == 0){
+            return 0;
+        } else {
+            return countryEntry.getPopulation() / countryEntry.getArea();
+        }
+    }
 }
--- src/main/java/de/bcxp/challenge/view/App.java
+++ src/main/java/de/bcxp/challenge/view/App.java
@@ -1,7 +1,9 @@
 package de.bcxp.challenge.view;
 
+import de.bcxp.challenge.controller.CountryController;
 import de.bcxp.challenge.controller.WeatherController;
 import de.bcxp.challenge.controller.handler.CSVEntryHandler;
+import de.bcxp.challenge.model.CountryEntry;
 import de.bcxp.challenge.model.WeatherEntry;
 
 /**
@@ -17,11 +19,12 @@ public final class App {
     public static void main(String... args) {
 
         // Your preparation code …
-        WeatherController weatherController = new WeatherController(new CSVEntryHandler< WeatherEntry >("src/main/resources/de/bcxp/challenge/weather.csv", ',', WeatherEntry.class));
+        WeatherController weatherController = new WeatherController(new CSVEntryHandler<>("src/main/resources/de/bcxp/challenge/weather.csv", ',', WeatherEntry.class));
         int dayWithSmallestTempSpread = weatherController.getSmallestTemperatureSpread();     // Your day analysis function call …
         System.out.printf("Day with smallest temperature spread: %s%n", dayWithSmallestTempSpread);
 
-        String countryWithHighestPopulationDensity = "Some country"; // Your population density analysis function call …
+        CountryController countryController = new CountryController(new CSVEntryHandler<>("src/main/resources/de/bcxp/challenge/countries.csv", ';', CountryEntry.class));
+        String countryWithHighestPopulationDensity = countryController.getDensestPopulatedCountry(); // Your population density analysis function call …
         System.out.printf("Country with highest population density: %s%n", countryWithHighestPopulationDensity);
     }
 }
